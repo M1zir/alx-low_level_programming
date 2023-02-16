ef7361c1e1c7e90dacc2197a58d34eeec0eb721c
M1zir <abubakra7mad@gmail.com>
2023-02-17 00:41:57 +0400

    alx

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,,
+printf("\"with proper grammar, but the outcome is a piece of art,,
 followed by a new line.\n"
 return (0);
 }
