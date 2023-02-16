2365bc328fa7bdea0d7eaaa7b72b4a78ae567b53
M1zir <abubakra7mad@gmail.com>
2023-02-17 00:30:16 +0400

    alx

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+*
+* Return: 0 on success, non-zero on failure
+*/
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,,
+		followed by a new line."
+return (0);
+}
