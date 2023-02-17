f1694aaaf3f830391944f12a67f45e2f4bd579a5
M1zir <abubakra7mad@gmail.com>
2023-02-18 00:50:02 +0400

    alx

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* main - Prints numbers between 0 to 9 with commas and spaces.
-* 
-* Return: Always 0 (Success)
+*main - Prints numbers between 0 to 9 with commas and spaces.
+*
+*Return: Always 0 (Success)
 */
 int main(void)
 {
